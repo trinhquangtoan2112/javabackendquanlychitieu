afd91d45d380778d881d07a6df8d399f1d0db367
trinhquangtoan2112 <trinhtoanquang@gmail.com>
2025-04-08 17:27:27 +0700

    .

--- src/main/java/com/data/toan/controller/TransactionController.java
+++ src/main/java/com/data/toan/controller/TransactionController.java
@@ -1,5 +1,13 @@
 package com.data.toan.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RequestMapping("/Transaction")
+@RestController
+@Slf4j
 public class TransactionController {
 
 }
